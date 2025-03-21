6e82d722cedc39b96bf3f96baa1363ca546c0206
Duncanyu <duncan.w.yu@gmail.com>
2025-03-21 10:46:18 -0400

    Merge

--- Assets/Scripts/Astriod/FallingObject.cs
+++ Assets/Scripts/Astriod/FallingObject.cs
@@ -6,16 +6,15 @@ using UnityEngine;
 public class FallingObject : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float fallSpeed = 0f; // Speed at which the object falls
+    public float fallSpeed = 8f; // Speed at which the object falls
     public PlayerMovement playerScript;
 
     
     void Update()
     {
-        // Move the object downwards over time
         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
     }
-    void OnTriggerEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Border")){
             Debug.Log("Hit border");
