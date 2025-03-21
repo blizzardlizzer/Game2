0fe5458b0ff96dcfd7492ec3ff307c43b18426bb
korokua <therealryanhu@gmail.com>
2025-03-21 10:45:48 -0400

    game menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +5,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("MainGame"); 
+        Debug.Log("Button clicked ");
+
     }
 }
